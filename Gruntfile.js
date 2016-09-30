bed0d662fa6f4ca02dfeb390d8576502294c86af
Ralf S. Engelschall <rse@engelschall.com>
2016-09-30 18:49:53 +0200

    upgrade dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
         },
         eslint: {
             options: {
-                config: "eslint.json"
+                configFile: "eslint.json"
             },
             "asty": [ "src/**/*.js", "tst/**/*.js" ]
         },
