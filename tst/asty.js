965f7f678ca7f8797e67d7cee15b96dea2148266
Ralf S. Engelschall <rse@engelschall.com>
2020-12-20 19:59:59 +0100

    make ESLint happy again

--- tst/asty.js
+++ tst/asty.js
@@ -27,32 +27,32 @@
 /* jshint -W030 */
 /* eslint no-unused-expressions: 0 */
 
-var chai = require("chai")
-var expect = chai.expect
+const chai = require("chai")
+const expect = chai.expect
 chai.config.includeStack = true
 
-var ASTy = require("../lib/asty.node.js")
+const ASTy = require("../lib/asty.node.js")
 
 describe("ASTy Library", function () {
     it("node base functionality", function () {
-        var asty = new ASTy()
-        var node = asty.create("foo")
+        const asty = new ASTy()
+        const node = asty.create("foo")
         expect(asty.isA(node)).to.be.true
         expect(node).to.be.a("object")
         expect(node).to.include.keys("T", "L", "A", "P", "C")
         expect(node.type()).to.be.equal("foo")
         expect(node).to.respondTo("type")
         expect(node).to.respondTo("dump")
-        var node2 = node.create("foo")
+        const node2 = node.create("foo")
         expect(asty.isA(node2)).to.be.true
     })
     it("node tree structure", function () {
-        var asty = new ASTy()
-        var node1 = asty.create("1")
-        var node11 = asty.create("1.1")
-        var node12 = asty.create("1.2")
-        var node121 = asty.create("1.2.1")
-        var node122 = asty.create("1.2.2")
+        const asty = new ASTy()
+        const node1 = asty.create("1")
+        const node11 = asty.create("1.1")
+        const node12 = asty.create("1.2")
+        const node121 = asty.create("1.2.1")
+        const node122 = asty.create("1.2.2")
         node1.add(node11, node12)
         node12.add(node121, node122)
         expect(node1.parent()).to.be.equal(null)
@@ -63,20 +63,20 @@ describe("ASTy Library", function () {
         expect(node122.parent()).to.be.equal(node12)
     })
     it("node extension functionality", function () {
-        var asty = new ASTy()
+        const asty = new ASTy()
         asty.extend({
             foo: function (arg) {
                 return "<" + arg + ">"
             }
         })
-        var node = asty.create("foo")
+        const node = asty.create("foo")
         expect(node).to.be.a("object")
         expect(node).to.respondTo("foo")
         expect(node.foo("bar")).to.be.equal("<bar>")
     })
     it("node serialize/unserialize functionality", function () {
-        var asty = new ASTy()
-        var node1 = asty.create("1")
+        const asty = new ASTy()
+        const node1 = asty.create("1")
         node1.set("foo", "bar")
         expect(node1.get("foo")).to.be.equal("bar")
         node1.unset("foo")
@@ -85,16 +85,16 @@ describe("ASTy Library", function () {
         node1.unset([ "foo" ])
         expect(node1.get("foo")).to.be.equal(undefined)
         node1.set("foo", { bar: 42, quux: "7" })
-        var node11 = asty.create("1.1")
-        var node12 = asty.create("1.2")
-        var node121 = asty.create("1.2.1")
-        var node122 = asty.create("1.2.2")
+        const node11 = asty.create("1.1")
+        const node12 = asty.create("1.2")
+        const node121 = asty.create("1.2.1")
+        const node122 = asty.create("1.2.2")
         node1.add(node11, node12)
         node12.add(node121, node122)
-        var dump1 = node1.dump()
-        var dump2 = ASTy.unserialize(ASTy.serialize(node1)).dump()
+        const dump1 = node1.dump()
+        const dump2 = ASTy.unserialize(ASTy.serialize(node1)).dump()
         expect(dump1).to.be.equal(dump2)
-        var dump3 = ASTy.unserialize(node1.serialize()).dump()
+        const dump3 = ASTy.unserialize(node1.serialize()).dump()
         expect(dump1).to.be.equal(dump3)
     })
 })
